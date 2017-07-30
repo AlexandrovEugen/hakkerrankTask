35013353538b1f5f216fce29c50beb8a8811035b
Евгений <evgeniy.alexandrov1994@gmail.com>
2017-07-30 23:37:47 +0300

    3 challenge: complete.

--- src/main/java/com/epam/lab/contacts/Solution.java
+++ src/main/java/com/epam/lab/contacts/Solution.java
@@ -1,7 +1,7 @@
 package com.epam.lab.contacts;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Solution {
@@ -9,10 +9,10 @@ public class Solution {
     public static void main(String[] args) {
         final Scanner in = new Scanner(System.in);
         final int n = in.nextInt();
-        if (n < 1 || n > 100_000){
+        if (n < 1 || n > 100_000) {
             throw new IllegalArgumentException();
         }
-        final List<String> contacts = new ArrayList<>(n);
+        final Trie trie = new Trie();
         for (int i = 0; i < n; i++) {
             final String op = in.next();
             final String contact = in.next();
@@ -21,14 +21,14 @@ public class Solution {
                     if (contact.length() < 1 || contact.length() > 21) {
                         throw new IllegalArgumentException();
                     } else {
-                        contacts.add(contact);
+                        trie.add(contact);
                     }
                     break;
                 case "find":
                     if (contact.length() < 1 || contact.length() > 21) {
                         throw new IllegalArgumentException();
                     }
-                    final int count = findPartialCount(contact, contacts);
+                    final int count = trie.find(contact);
                     System.out.println(count);
                     break;
                 default:
@@ -37,20 +37,44 @@ public class Solution {
         }
     }
 
-    private static int findPartialCount(String partial, List<String> contacts) {
-        int count = 0;
-        if (contacts.isEmpty()){
-            return 0;
+    static class Node {
+
+        private final Map<Character, Node> children = new HashMap<>();
+        private int size;
+
+        private void putChildIfAbsent(char ch) {
+            children.putIfAbsent(ch, new Node());
         }
-        else {
-            for (String contact: contacts) {
-                if (partial.length() <= contact.length()){
-                    if (contact.startsWith(partial)){
-                        count = count + 1;
-                    }
-                }
+
+        private Node getChild(char ch) {
+            return children.get(ch);
+        }
+    }
+
+    private static class Trie {
+
+        final private Node root = new Node();
+
+        private void add(String str) {
+            Node curr = root;
+            for (int i = 0; i < str.length(); i++) {
+                Character ch = str.charAt(i);
+                curr.putChildIfAbsent(ch);
+                curr = curr.getChild(ch);
+                curr.size++;
             }
         }
-        return count;
+
+        private int find(String partial) {
+            Node curr = root;
+            for (int i = 0; i < partial.length(); i++) {
+                final Character ch = partial.charAt(i);
+                curr = curr.getChild(ch);
+                if (curr == null) {
+                    return 0;
+                }
+            }
+            return curr.size;
+        }
     }
 }
